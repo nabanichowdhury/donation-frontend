55a402116b6bbef03cc04b4fee6469f56e7901e9
developerashikur1 <masik4506@gmail.com>
2024-03-13 00:11:45 +0600

    file added

--- src/components/ui/HelpForm/HelpForm.js
+++ src/components/ui/HelpForm/HelpForm.js
@@ -2,23 +2,24 @@
 
 import { useForm } from "react-hook-form";
 
+const input = "input input-bordered w-full rounded-xl";
+const file = "file-input file-input-bordered w-full";
+const textarea = "textarea textarea-bordered w-full rounded-xl min-h-32 resize-y";
+const select = "select select-bordered w-full rounded-xl";
+
 const helpInputs = [
-    {
-        title: 'Email',
-        name: 'email',
-        type: 'text',
-        options: null,
-    },
     {
         title: 'Subject',
         name: 'subject',
         type: 'text',
         options: null,
+        class: input,
     },
     {
         title: 'Category',
         name: 'category',
         type: 'select',
+        class: select,
         options: [
             {
                 title:'Health',
@@ -38,17 +39,26 @@ const helpInputs = [
             },
         ]
     },
-    // {
-    //     title: 'Image',
-    //     name: 'image',
-    //     type: 'file',
-    //     options: null,
-    // },
+    {
+        title: 'Email',
+        name: 'email',
+        type: 'email',
+        options: null,
+        class: input,
+    },
+    {
+        title: 'Image',
+        name: 'image',
+        type: 'file',
+        options: null,
+        class: file,
+    },
     {
         title: 'Description',
         name: 'description',
         type: 'textarea',
         options: null,
+        class: textarea,
     },
 ]
 
@@ -70,11 +80,11 @@ const HelpForm = () => {
                 return(
                     <div key={item.name} className="">
                         <h4 className="mb-2">{item.title}</h4>
-                        {item.type === "text" && <input {...register(item.name, {required: `${item.title} is required.`})} type={item.type} placeholder={item.title} className="input input-bordered w-full rounded-xl" />}
-                        {item.type === "select" && <select {...register(item.name, {required: `${item.title} is required.`})} className="select select-bordered w-full rounded-xl">
+                        {(item.type !== "select") && (item.type !== "textarea") && <input {...register(item.name, {required: `${item.title} is required.`})} type={item.type} placeholder={item.title} className={item.class} />}
+                        {item.type === "select" && <select {...register(item.name, {required: `${item.title} is required.`})} className={item.class}>
                             {item.options !== null && item.options.map(category => <option key={category.value} value={category.value} className="py-3 h-11">{category.title}</option>)}
                         </select>}
-                        {item.type === "textarea" && <textarea  {...register(item.name, {required: `${item.title} is required.`})} className="textarea textarea-bordered w-full rounded-xl min-h-32 resize-y" placeholder={item.title} />}
+                        {item.type === "textarea" && <textarea  {...register(item.name, {required: `${item.title} is required.`})} className={item.class} placeholder={item.title} />}
                     </div>
                 )})}
                 <button onClick={handleSubmit(handleSubmitForm)} className="btn bg-pink-400 text-white hover:bg-green-400 rounded-xl">Submit</button>
